06e6d91a7e5ed2b6688b75d3eac87b641d65d4b9
gadlack1 <76732035+gadlack1@users.noreply.github.com>
2021-07-30 18:57:28 +0700

    Create UserAddForm.Designer.cs

---
+++ Main Form/Main Form/UserAddForm.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Main_Form
+{
+    partial class UserAddForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.UserTextBox = new System.Windows.Forms.TextBox();
+            this.OkBtn = new System.Windows.Forms.Button();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PasswdTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.RightsComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Имя пользователя";
+            // 
+            // UserTextBox
+            // 
+            this.UserTextBox.Location = new System.Drawing.Point(13, 30);
+            this.UserTextBox.Name = "UserTextBox";
+            this.UserTextBox.Size = new System.Drawing.Size(241, 20);
+            this.UserTextBox.TabIndex = 1;
+            // 
+            // OkBtn
+            // 
+            this.OkBtn.Location = new System.Drawing.Point(12, 210);
+            this.OkBtn.Name = "OkBtn";
+            this.OkBtn.Size = new System.Drawing.Size(75, 23);
+            this.OkBtn.TabIndex = 2;
+            this.OkBtn.Text = "ОК";
+            this.OkBtn.UseVisualStyleBackColor = true;
+            this.OkBtn.Click += new System.EventHandler(this.OkBtn_Click);
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancelBtn.Location = new System.Drawing.Point(179, 210);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.CancelBtn.TabIndex = 3;
+            this.CancelBtn.Text = "Отмена";
+            this.CancelBtn.UseVisualStyleBackColor = true;
+            this.CancelBtn.Click += new System.EventHandler(this.CancelBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Пароль";
+            // 
+            // PasswdTextBox
+            // 
+            this.PasswdTextBox.Location = new System.Drawing.Point(12, 85);
+            this.PasswdTextBox.Name = "PasswdTextBox";
+            this.PasswdTextBox.Size = new System.Drawing.Size(242, 20);
+            this.PasswdTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Права";
+            // 
+            // RightsComboBox
+            // 
+            this.RightsComboBox.FormattingEnabled = true;
+            this.RightsComboBox.Location = new System.Drawing.Point(13, 148);
+            this.RightsComboBox.Name = "RightsComboBox";
+            this.RightsComboBox.Size = new System.Drawing.Size(241, 21);
+            this.RightsComboBox.TabIndex = 7;
+            // 
+            // UserAddForm
+            // 
+            this.AcceptButton = this.OkBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.CancelBtn;
+            this.ClientSize = new System.Drawing.Size(266, 246);
+            this.Controls.Add(this.RightsComboBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.PasswdTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CancelBtn);
+            this.Controls.Add(this.OkBtn);
+            this.Controls.Add(this.UserTextBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "UserAddForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Добавление пользователя";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UserAddForm_FormClosing);
+            this.Load += new System.EventHandler(this.UserAddForm_Load);
+            this.Shown += new System.EventHandler(this.UserAddForm_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox UserTextBox;
+        private System.Windows.Forms.Button OkBtn;
+        private System.Windows.Forms.Button CancelBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox PasswdTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox RightsComboBox;
+    }
+}
