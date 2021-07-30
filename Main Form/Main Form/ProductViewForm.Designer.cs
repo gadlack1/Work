ae649f38abd4ff7a683e3bc3a6a448d68e8b86b8
gadlack1 <76732035+gadlack1@users.noreply.github.com>
2021-07-30 18:56:51 +0700

    Create ProductViewForm.Designer.cs

---
+++ Main Form/Main Form/ProductViewForm.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Main_Form
+{
+    partial class ProductViewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ProductDGV = new System.Windows.Forms.DataGridView();
+            this.ProductType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Warehouse = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Unit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MinStock = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Expected = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CloseBtn = new System.Windows.Forms.Button();
+            this.DeleteBtn = new System.Windows.Forms.Button();
+            this.EditBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductDGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ProductDGV
+            // 
+            this.ProductDGV.AllowUserToAddRows = false;
+            this.ProductDGV.AllowUserToDeleteRows = false;
+            this.ProductDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ProductDGV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ProductType,
+            this.Price,
+            this.Warehouse,
+            this.Unit,
+            this.MinStock,
+            this.Expected,
+            this.ID});
+            this.ProductDGV.Location = new System.Drawing.Point(22, 26);
+            this.ProductDGV.Name = "ProductDGV";
+            this.ProductDGV.ReadOnly = true;
+            this.ProductDGV.RowHeadersWidth = 20;
+            this.ProductDGV.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.ProductDGV.Size = new System.Drawing.Size(840, 150);
+            this.ProductDGV.TabIndex = 0;
+            this.ProductDGV.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ProductDGV_CellDoubleClick);
+            // 
+            // ProductType
+            // 
+            this.ProductType.HeaderText = "Тип продукции";
+            this.ProductType.Name = "ProductType";
+            this.ProductType.ReadOnly = true;
+            this.ProductType.Width = 200;
+            // 
+            // Price
+            // 
+            this.Price.HeaderText = "Цена";
+            this.Price.Name = "Price";
+            this.Price.ReadOnly = true;
+            // 
+            // Warehouse
+            // 
+            this.Warehouse.HeaderText = "Склад";
+            this.Warehouse.Name = "Warehouse";
+            this.Warehouse.ReadOnly = true;
+            // 
+            // Unit
+            // 
+            this.Unit.HeaderText = "Единица измерения";
+            this.Unit.Name = "Unit";
+            this.Unit.ReadOnly = true;
+            this.Unit.Width = 150;
+            // 
+            // MinStock
+            // 
+            this.MinStock.HeaderText = "Минимальный запас";
+            this.MinStock.Name = "MinStock";
+            this.MinStock.ReadOnly = true;
+            // 
+            // Expected
+            // 
+            this.Expected.HeaderText = "Ожидается";
+            this.Expected.Name = "Expected";
+            this.Expected.ReadOnly = true;
+            this.Expected.Width = 150;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            this.ID.Visible = false;
+            // 
+            // CloseBtn
+            // 
+            this.CloseBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CloseBtn.Location = new System.Drawing.Point(416, 213);
+            this.CloseBtn.Name = "CloseBtn";
+            this.CloseBtn.Size = new System.Drawing.Size(75, 23);
+            this.CloseBtn.TabIndex = 1;
+            this.CloseBtn.Text = "Закрыть";
+            this.CloseBtn.UseVisualStyleBackColor = true;
+            this.CloseBtn.Click += new System.EventHandler(this.CloseBtn_Click);
+            // 
+            // DeleteBtn
+            // 
+            this.DeleteBtn.Location = new System.Drawing.Point(278, 213);
+            this.DeleteBtn.Name = "DeleteBtn";
+            this.DeleteBtn.Size = new System.Drawing.Size(75, 23);
+            this.DeleteBtn.TabIndex = 2;
+            this.DeleteBtn.Text = "Удалить";
+            this.DeleteBtn.UseVisualStyleBackColor = true;
+            this.DeleteBtn.Click += new System.EventHandler(this.DeleteBtn_Click);
+            // 
+            // EditBtn
+            // 
+            this.EditBtn.Location = new System.Drawing.Point(544, 213);
+            this.EditBtn.Name = "EditBtn";
+            this.EditBtn.Size = new System.Drawing.Size(75, 23);
+            this.EditBtn.TabIndex = 3;
+            this.EditBtn.Text = "Изменить";
+            this.EditBtn.UseVisualStyleBackColor = true;
+            this.EditBtn.Click += new System.EventHandler(this.EditBtn_Click);
+            // 
+            // ProductViewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.CloseBtn;
+            this.ClientSize = new System.Drawing.Size(887, 263);
+            this.Controls.Add(this.EditBtn);
+            this.Controls.Add(this.DeleteBtn);
+            this.Controls.Add(this.CloseBtn);
+            this.Controls.Add(this.ProductDGV);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ProductViewForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Просмотр продукции";
+            this.Activated += new System.EventHandler(this.ProductViewForm_Activated);
+            ((System.ComponentModel.ISupportInitialize)(this.ProductDGV)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView ProductDGV;
+        private System.Windows.Forms.Button CloseBtn;
+        private System.Windows.Forms.Button DeleteBtn;
+        private System.Windows.Forms.Button EditBtn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ProductType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Warehouse;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Unit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MinStock;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Expected;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+    }
+}
