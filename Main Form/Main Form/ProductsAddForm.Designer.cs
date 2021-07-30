5ac96c776cbf7b034d0697fde4f4d708f691f6ef
gadlack1 <76732035+gadlack1@users.noreply.github.com>
2021-07-30 18:54:32 +0700

    Create ProductsAddForm.Designer.cs

---
+++ Main Form/Main Form/ProductsAddForm.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Main_Form
+{
+    partial class ProductsAddForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.UnitTextBox = new System.Windows.Forms.TextBox();
+            this.WarehourseTextBox = new System.Windows.Forms.TextBox();
+            this.MinStockTextBox = new System.Windows.Forms.TextBox();
+            this.PriceTextBox = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.OkBtn = new System.Windows.Forms.Button();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.ProductComboBox = new System.Windows.Forms.ComboBox();
+            this.ExpectedDTP = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(295, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Единица измерения";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(295, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Цена";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Склад";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(295, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ожидается";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Минимальный запас";
+            // 
+            // UnitTextBox
+            // 
+            this.UnitTextBox.Location = new System.Drawing.Point(298, 103);
+            this.UnitTextBox.Name = "UnitTextBox";
+            this.UnitTextBox.Size = new System.Drawing.Size(211, 20);
+            this.UnitTextBox.TabIndex = 6;
+            // 
+            // WarehourseTextBox
+            // 
+            this.WarehourseTextBox.Location = new System.Drawing.Point(15, 102);
+            this.WarehourseTextBox.Name = "WarehourseTextBox";
+            this.WarehourseTextBox.Size = new System.Drawing.Size(211, 20);
+            this.WarehourseTextBox.TabIndex = 7;
+            // 
+            // MinStockTextBox
+            // 
+            this.MinStockTextBox.Location = new System.Drawing.Point(15, 179);
+            this.MinStockTextBox.Name = "MinStockTextBox";
+            this.MinStockTextBox.Size = new System.Drawing.Size(211, 20);
+            this.MinStockTextBox.TabIndex = 8;
+            // 
+            // PriceTextBox
+            // 
+            this.PriceTextBox.Location = new System.Drawing.Point(298, 25);
+            this.PriceTextBox.Name = "PriceTextBox";
+            this.PriceTextBox.Size = new System.Drawing.Size(211, 20);
+            this.PriceTextBox.TabIndex = 9;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(69, 13);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "Тип продукт";
+            // 
+            // OkBtn
+            // 
+            this.OkBtn.Location = new System.Drawing.Point(15, 246);
+            this.OkBtn.Name = "OkBtn";
+            this.OkBtn.Size = new System.Drawing.Size(75, 23);
+            this.OkBtn.TabIndex = 10;
+            this.OkBtn.Text = "ОК";
+            this.OkBtn.UseVisualStyleBackColor = true;
+            this.OkBtn.Click += new System.EventHandler(this.OkBtn_Click);
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancelBtn.Location = new System.Drawing.Point(433, 246);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.CancelBtn.TabIndex = 11;
+            this.CancelBtn.Text = "Отмена";
+            this.CancelBtn.UseVisualStyleBackColor = true;
+            this.CancelBtn.Click += new System.EventHandler(this.CancelBtn_Click);
+            // 
+            // ProductComboBox
+            // 
+            this.ProductComboBox.FormattingEnabled = true;
+            this.ProductComboBox.Location = new System.Drawing.Point(15, 23);
+            this.ProductComboBox.Name = "ProductComboBox";
+            this.ProductComboBox.Size = new System.Drawing.Size(211, 21);
+            this.ProductComboBox.TabIndex = 12;
+            // 
+            // ExpectedDTP
+            // 
+            this.ExpectedDTP.Location = new System.Drawing.Point(298, 179);
+            this.ExpectedDTP.Name = "ExpectedDTP";
+            this.ExpectedDTP.Size = new System.Drawing.Size(210, 20);
+            this.ExpectedDTP.TabIndex = 13;
+            // 
+            // ProductsAddForm
+            // 
+            this.AcceptButton = this.OkBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.CancelBtn;
+            this.ClientSize = new System.Drawing.Size(545, 286);
+            this.Controls.Add(this.ExpectedDTP);
+            this.Controls.Add(this.ProductComboBox);
+            this.Controls.Add(this.CancelBtn);
+            this.Controls.Add(this.OkBtn);
+            this.Controls.Add(this.PriceTextBox);
+            this.Controls.Add(this.MinStockTextBox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.WarehourseTextBox);
+            this.Controls.Add(this.UnitTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ProductsAddForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Добавление продукта";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ProductsAddForm_FormClosing);
+            this.Shown += new System.EventHandler(this.ProductsAddForm_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox UnitTextBox;
+        private System.Windows.Forms.TextBox WarehourseTextBox;
+        private System.Windows.Forms.TextBox MinStockTextBox;
+        private System.Windows.Forms.TextBox PriceTextBox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button OkBtn;
+        private System.Windows.Forms.Button CancelBtn;
+        private System.Windows.Forms.ComboBox ProductComboBox;
+        private System.Windows.Forms.DateTimePicker ExpectedDTP;
+    }
+}
