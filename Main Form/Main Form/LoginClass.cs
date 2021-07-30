a13beb98fad03017cc8bcd11e6078816145c24a9
gadlack1 <76732035+gadlack1@users.noreply.github.com>
2021-07-30 18:47:13 +0700

    Create LoginClass.cs

---
+++ Main Form/Main Form/LoginClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Main_Form
+{
+    class LoginClass
+    {
+        public static bool AdminFlag;
+        public static bool MainSuccessFlag;
+        public static bool LSF;
+        public static string LogOutUser;
+        public static bool LT;
+    }
+}
